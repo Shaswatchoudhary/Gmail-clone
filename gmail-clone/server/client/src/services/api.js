a9b60dc81f3d9eaf36da71f18a4683185d1ac37e
Shaswat <kaushikchoudhary33@icloud.com>
2025-05-11 23:06:17 +0530

    Update api.js

--- gmail-clone/server/client/src/services/api.js
+++ gmail-clone/server/client/src/services/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-// Use the value from .env
 const API_URI = process.env.REACT_APP_API_URI;
 
 const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
     const { params, urlParams, ...body } = requestData;
 
-    // Remove trailing slash from API_URI if it exists
     const baseUrl = API_URI.endsWith('/') ? API_URI.slice(0, -1) : API_URI;
     const endpoint = serviceUrlObject.endpoint.startsWith('/') ? serviceUrlObject.endpoint.slice(1) : serviceUrlObject.endpoint;
     const typePath = type ? `/${type}` : '';
@@ -14,7 +12,12 @@ const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
     return await axios({
         method: serviceUrlObject.method,
         url: `${baseUrl}/${endpoint}${typePath}`,
-        data: requestData
+        data: requestData,
+        headers: {
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Expires': '0',
+        },
     });
 };
 
