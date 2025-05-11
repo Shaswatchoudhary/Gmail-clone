53be9a636b19ad5a75bfc384be5dd42321bc97b5
Shaswat <kaushikchoudhary33@icloud.com>
2025-05-11 19:24:05 +0530

    final

--- gmail-clone/server/client/src/services/api.js
+++ gmail-clone/server/client/src/services/api.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-const API_URI = '';
+// ✅ Use the value from .env
+const API_URI = process.env.REACT_APP_API_URI;
 
 const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
     const { params, urlParams, ...body } = requestData;
 
     return await axios({
         method: serviceUrlObject.method,
-        url: `${API_URI}/${serviceUrlObject.endpoint}/${type}`,
+        url: `${API_URI}/${serviceUrlObject.endpoint}${type ? `/${type}` : ''}`,
         data: requestData
-    })
-}
+    });
+};
 
 export default API_GMAIL;
